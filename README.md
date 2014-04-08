79c06c23c12bcd7b5cdc0d5b583d38af9ea0a103
papix <mail@papix.net>
2014-04-08 13:08:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Reply::Plugin::ORM - It's new $module
+
+# SYNOPSIS
+
+    use Reply::Plugin::ORM;
+
+# DESCRIPTION
+
+Reply::Plugin::ORM is ...
+
+# LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+papix <mail@papix.net>
---
+++ lib/Reply/Plugin/ORM.pm
@@ -0,0 +1,39 @@
+package Reply::Plugin::ORM;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Reply::Plugin::ORM - It's new $module
+
+=head1 SYNOPSIS
+
+    use Reply::Plugin::ORM;
+
+=head1 DESCRIPTION
+
+Reply::Plugin::ORM is ...
+
+=head1 LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+papix E<lt>mail@papix.netE<gt>
+
+=cut
+
